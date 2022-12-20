e877454917d338d67d47f24a77142c04e4b793b7
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-20 21:04:07 +0900

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author authorPushkinAS = new Author("Александр Сергеевич Пушкин");
+        Author authorTolstoyLN = new Author("Лев Николаевич Толстой");
+        Author authorShildtG = new Author("Gerbert Shildt");
+
+        Book bookSaltan = new Book(authorPushkinAS.getName(), 1831);
+        Book bookWar = new Book(authorTolstoyLN.getName(), 1873);
+        Book bookJava = new Book(authorShildtG.getName(), 2021);
+
+        bookJava.setYear(2022);
+
     }
 }
