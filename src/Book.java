513a92524b74a95fd2e7ecdcc9f9e4b7bcab80af
Anton Saltanov <saltanov.anton@yandex.ru>
2022-12-22 23:40:11 +0900

    Homework 2

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private Author author;
     private String nameBook;
@@ -19,5 +21,31 @@ public class Book {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author=" + author +
+                ", nameBook='" + nameBook + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(author, book.author) && Objects.equals(nameBook, book.nameBook);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, nameBook, year);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,18 @@ public class Main {
         Author authorPushkinAS = new Author("Александр", "Пушкин");
         Author authorTolstoyLN = new Author("Лев", "Толстой");
         Author authorShildtG = new Author("Gerbert", "Shildt");
+        Author authorPushkinAS2 = new Author("Александр", "Пушкин");
 
         Book bookSaltan = new Book(authorPushkinAS, "Сказко о царе Салтане", 1831);
         Book bookWar = new Book(authorTolstoyLN, "Война и Мир", 1873);
         Book bookJava = new Book(authorShildtG, "Java - полное руководство", 2021);
 
         bookJava.setYear(2022);
+        System.out.println(bookSaltan.toString());
+
+        System.out.println(authorPushkinAS.equals(authorPushkinAS2));
+        System.out.println(authorPushkinAS.hashCode());
+        System.out.println(authorPushkinAS2.hashCode());
 
     }
 }
